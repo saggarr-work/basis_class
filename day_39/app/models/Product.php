1aef4f8b9a91b38c8b04ec5e5d94bba10f09b672
sagar <saggarr.work@gmail.com>
2023-06-06 17:26:01 +0600

    day_39 class added

---
+++ day_39/app/models/Product.php
@@ -0,0 +1,29 @@
+<?php
+    namespace App\models;
+    
+    class Product{
+
+        public $products = [
+            [
+                "title" => "Men's Premium soft and comfortable T-shirt - Fabric's White",
+                "image" => "https://static-01.daraz.com.bd/p/715b021430856e83846d2d69af664469.jpg_720x720.jpg_.webp",
+                "price" => [
+                    "regular" => 200,
+                    "offer" => 150
+                ],
+                "short_des" => "The men's product is best",
+                "long_des" => "Men's Product details of Men's Premium soft and comfortable T-shirt - Fabric's White"
+            ],
+            [
+                "title" => "Women's Premium soft and comfortable T-shirt - Fabric's White",
+                "image" => "https://static-01.daraz.com.bd/p/9e2361893dfc53f057a56e820cd4e126.jpg_720x720.jpg_.webp",
+                "price" => [
+                    "regular" => 500,
+                    "offer" => 498
+                ],
+                "short_des" => "The women's product is best",
+                "long_des" => "Women's Product details of Men's Premium soft and comfortable T-shirt - Fabric's White"
+            ]
+        ];
+    }
+?>
