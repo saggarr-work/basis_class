d6af1cb1c368073d4a2164afab1b840aaf7849a2
sagar <saggarr.work@gmail.com>
2023-06-12 17:36:36 +0600

    two template added and mastering the template

---
+++ laravel/day_3/o-shop/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ShopController;
+use App\Http\Controllers\SingleProductController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index']);
+Route::get('/cart', [CartController::class, 'index']);
+Route::get('/checkout', [CheckoutController::class, 'index']);
+Route::get('/shop', [ShopController::class, 'index']);
+Route::get('/single-product', [SingleProductController::class, 'index']);
